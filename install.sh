63807191af9ca482e08e7107c4c9c74808176bb1
Ankit Vadehra <ankitvad@gmail.com>
2015-02-01 11:19:40 +0530

    Have to Update the dir location

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+#Part of goto: cd replacement tool.
+#By: Ankit Vadehra
+#Contact: ankitvad[at]gmail[dot[com]
+#License: Standard MIT License.
 INSTALL=~/.local/bin
 BASH_FILE=~/.bashrc
 bold=`tput bold`
 normal=`tput sgr0`
 mkdir -p "$INSTALL"
 cp goto "$INSTALL"
-echo 'export PATH=$PATH:/home/ankitvad/.local/bin/' >> "$BASH_FILE"
+echo 'export PATH=$PATH:~/.local/bin/' >> "$BASH_FILE"
 echo 'alias goto=". goto"' >> "$BASH_FILE"
 echo -e "Added Stuff in .bashrc"
 . ~/.bashrc
