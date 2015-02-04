66d41c102ab6b141db6ea0e414be8643be54e304
Ankit Vadehra <ankitvad@gmail.com>
2015-02-04 16:13:58 +0530

    Adding support for people using .bash_profile. Thanks to @muya for pointing it out

--- install.sh
+++ install.sh
@@ -5,12 +5,24 @@
 #License: Standard MIT License.
 INSTALL=~/.local/bin
 BASH_FILE=~/.bashrc
+#for those on bash_profile.
+BASH_FILE2=~/.bash_profile
 bold=`tput bold`
 normal=`tput sgr0`
 mkdir -p "$INSTALL"
 cp goto "$INSTALL"
-echo 'export PATH=$PATH:~/.local/bin/' >> "$BASH_FILE"
-echo 'alias goto=". goto"' >> "$BASH_FILE"
-echo -e "Added Stuff in .bashrc"
+#For people using .bashrc.
+if [ -f "$BASH_FILE" ]; then
+	echo 'export PATH=$PATH:~/.local/bin/' >> "$BASH_FILE"
+	echo 'alias goto=". goto"' >> "$BASH_FILE"
+	echo -e "Added Stuff in .bashrc"
+fi
+#Creating new bash_profile file, if doesnt exist. if exists, the appending
+#value. Some people on MAC don't use .bashrc.
+if [ -f "$BASH_FILE2" ]; then
+	echo 'export PATH=$PATH:~/.local/bin/' >> "$BASH_FILE2"
+	echo 'alias goto=". goto"' >> "$BASH_FILE2"
+	echo -e "Added Stuff in .bash_profile"
+fi	
 . ~/.bashrc
 exit 0
