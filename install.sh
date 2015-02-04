592c949647434ff541d33a1542a921f5f3ce2653
Muya <kingkonig@gmail.com>
2015-02-04 12:43:24 +0300

    add support for adding to .bash_profile on Mac OS

--- install.sh
+++ install.sh
@@ -5,12 +5,21 @@
 #License: Standard MIT License.
 INSTALL=~/.local/bin
 BASH_FILE=~/.bashrc
+MAC_OS_BASH_FILE=~/.bash_profile
 bold=`tput bold`
 normal=`tput sgr0`
 mkdir -p "$INSTALL"
 cp goto "$INSTALL"
-echo 'export PATH=$PATH:~/.local/bin/' >> "$BASH_FILE"
-echo 'alias goto=". goto"' >> "$BASH_FILE"
-echo -e "Added Stuff in .bashrc"
-. ~/.bashrc
+
+if [ -f $MAC_OS_BASH_FILE ]; then
+  echo 'export PATH=$PATH:~/.local/bin/' >> "$MAC_OS_BASH_FILE"
+  echo 'alias goto=". goto"' >> "$MAC_OS_BASH_FILE"
+  echo -e "Added Stuff in .bash_profile"
+  . ~/.bash_profile
+else
+  echo 'export PATH=$PATH:~/.local/bin/' >> "$BASH_FILE"
+  echo 'alias goto=". goto"' >> "$BASH_FILE"
+  echo -e "Added Stuff in .bashrc"
+  . ~/.bashrc
+fi
 exit 0
