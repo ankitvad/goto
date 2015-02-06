3d88df6edca405918ac5f45cafe8554616b7c1b5
Ankit Vadehra <ankitvad@gmail.com>
2015-02-06 23:18:17 +0530

    mandb fails for a MAC. Updating the condition for that, thanks Drew Milis for intimating

--- install.sh
+++ install.sh
@@ -31,7 +31,17 @@ echo -e "root/sudo privilege might be required? (yes/no)"
 read choice
 if [ "$choice" = "yes" ] || [ "$choice" = "y" ] ; then
 	sudo cp "$(pwd)/man_page/goto.1" /usr/local/share/man/man1/
-	sudo mandb
+	#I was just notified by 'Drew Mills' on Google+ that the command
+	#'mandb' does not exist for a MAC system. Since, this file
+	#copies the man-page in the proper location anyways, it seems
+	#that a MAC system is able to pick up the new man page automatically.
+	#No Manual 'mandb' required. Hence adding a condition to check for
+	#'mandb's existence, and then executing it. So, that errors don't creep up.
+	#Thank's Drew. :)
+	#Checking for mandb :
+	if type "mandb" > /dev/null 2>&1; then
+		sudo mandb	
+	fi	
 	echo -e " "
 	echo -e "Done. Added man page. To access: 'man goto'"	
 else
